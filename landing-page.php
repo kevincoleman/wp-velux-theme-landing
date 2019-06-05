103c957bf56d6341e9f622b61892164498904377
kevincoleman <kevin.n.coleman@gmail.com>
2019-06-05 14:20:24 -0700

    Fix CSS path

--- landing-page.php
+++ landing-page.php
@@ -20,7 +20,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<link rel="stylesheet" type="text/css" href="/wp-content/themes/primer/landing-style.css">
+	<link rel="stylesheet" type="text/css" href="/wp-content/themes/velux/landing-style.css">
 	<?php wp_head(); ?>
 </head>
 
